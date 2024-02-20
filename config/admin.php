7718b2c35ace84e58a3fca5b2ee70aeb159aefcc
g.dolgushin <g.dolgushin@mcplat.ru>
2024-02-21 00:41:59 +0300

    update project

--- config/admin.php
+++ config/admin.php
@@ -440,7 +440,7 @@ return [
         ],
         'material-ui' => [
             // If the value is set to false, this extension will be disabled
-            'enable' => true
+            'enable' => false
         ],
         'multi-language' => [
             'enable' => true,
