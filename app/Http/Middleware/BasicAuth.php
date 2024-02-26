48c075e36f15f4a53a1f8e1f3f8e88e9a6c4709b
g.dolgushin <40915075+DolgushinG@users.noreply.github.com>
2024-02-26 19:26:36 +0300

    Update BasicAuth.php

--- app/Http/Middleware/BasicAuth.php
+++ app/Http/Middleware/BasicAuth.php
@@ -17,7 +17,7 @@ class BasicAuth
     public function handle($request, Closure $next)
     {
         $AUTH_USER = 'owner';
-        $AUTH_PASS = 'climber';
+        $AUTH_PASS = 'owner';
         header('Cache-Control: no-cache, must-revalidate, max-age=0');
         $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
         $is_not_authenticated = (
