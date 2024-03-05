bea4784b1ea124dcc3089aa5d2490756f7139e02
g.dolgushin <g.dolgushin@mcplat.ru>
2024-03-05 17:38:38 +0300

    update project

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -422,177 +422,38 @@
 {{--            </div>--}}
 {{--        </section><!-- End Testimonials Section -->--}}
 
-{{--        <!-- ======= Portfolio Section ======= -->--}}
-{{--        <section id="portfolio" class="portfolio">--}}
-{{--            <div class="container" data-aos="fade-up">--}}
-
-{{--                <div class="section-title">--}}
-{{--                    <h2>Portfolio</h2>--}}
-{{--                    <h3>Check our <span>Portfolio</span></h3>--}}
-{{--                    <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas--}}
-{{--                        atque vitae autem.</p>--}}
-{{--                </div>--}}
-
-{{--                <div class="row" data-aos="fade-up" data-aos-delay="100">--}}
-{{--                    <div class="col-lg-12 d-flex justify-content-center">--}}
-{{--                        <ul id="portfolio-flters">--}}
-{{--                            <li data-filter="*" class="filter-active">All</li>--}}
-{{--                            <li data-filter=".filter-app">App</li>--}}
-{{--                            <li data-filter=".filter-card">Card</li>--}}
-{{--                            <li data-filter=".filter-web">Web</li>--}}
-{{--                        </ul>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-
-{{--                <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">--}}
-
-{{--                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">--}}
-{{--                        <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">--}}
-{{--                        <div class="portfolio-info">--}}
-{{--                            <h4>App 1</h4>--}}
-{{--                            <p>App</p>--}}
-{{--                            <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery"--}}
-{{--                               class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>--}}
-{{--                            <a href="portfolio-details.html" class="details-link" title="More Details"><i--}}
-{{--                                    class="bx bx-link"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-
-{{--                    <div class="col-lg-4 col-md-6 portfolio-item filter-web">--}}
-{{--                        <img src="assets/img/portfolio/portfolio-2.jpg" class="img-fluid" alt="">--}}
-{{--                        <div class="portfolio-info">--}}
-{{--                            <h4>Web 3</h4>--}}
-{{--                            <p>Web</p>--}}
-{{--                            <a href="assets/img/portfolio/portfolio-2.jpg" data-gallery="portfolioGallery"--}}
-{{--                               class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>--}}
-{{--                            <a href="portfolio-details.html" class="details-link" title="More Details"><i--}}
-{{--                                    class="bx bx-link"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-
-{{--                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">--}}
-{{--                        <img src="assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">--}}
-{{--                        <div class="portfolio-info">--}}
-{{--                            <h4>App 2</h4>--}}
-{{--                            <p>App</p>--}}
-{{--                            <a href="assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery"--}}
-{{--                               class="portfolio-lightbox preview-link" title="App 2"><i class="bx bx-plus"></i></a>--}}
-{{--                            <a href="portfolio-details.html" class="details-link" title="More Details"><i--}}
-{{--                                    class="bx bx-link"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-
-{{--                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">--}}
-{{--                        <img src="assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">--}}
-{{--                        <div class="portfolio-info">--}}
-{{--                            <h4>Card 2</h4>--}}
-{{--                            <p>Card</p>--}}
-{{--                            <a href="assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery"--}}
-{{--                               class="portfolio-lightbox preview-link" title="Card 2"><i class="bx bx-plus"></i></a>--}}
-{{--                            <a href="portfolio-details.html" class="details-link" title="More Details"><i--}}
-{{--                                    class="bx bx-link"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-
-{{--                    <div class="col-lg-4 col-md-6 portfolio-item filter-web">--}}
-{{--                        <img src="assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">--}}
-{{--                        <div class="portfolio-info">--}}
-{{--                            <h4>Web 2</h4>--}}
-{{--                            <p>Web</p>--}}
-{{--                            <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery"--}}
-{{--                               class="portfolio-lightbox preview-link" title="Web 2"><i class="bx bx-plus"></i></a>--}}
-{{--                            <a href="portfolio-details.html" class="details-link" title="More Details"><i--}}
-{{--                                    class="bx bx-link"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-
-{{--                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">--}}
-{{--                        <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">--}}
-{{--                        <div class="portfolio-info">--}}
-{{--                            <h4>App 3</h4>--}}
-{{--                            <p>App</p>--}}
-{{--                            <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery"--}}
-{{--                               class="portfolio-lightbox preview-link" title="App 3"><i class="bx bx-plus"></i></a>--}}
-{{--                            <a href="portfolio-details.html" class="details-link" title="More Details"><i--}}
-{{--                                    class="bx bx-link"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-
-{{--                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">--}}
-{{--                        <img src="assets/img/portfolio/portfolio-7.jpg" class="img-fluid" alt="">--}}
-{{--                        <div class="portfolio-info">--}}
-{{--                            <h4>Card 1</h4>--}}
-{{--                            <p>Card</p>--}}
-{{--                            <a href="assets/img/portfolio/portfolio-7.jpg" data-gallery="portfolioGallery"--}}
-{{--                               class="portfolio-lightbox preview-link" title="Card 1"><i class="bx bx-plus"></i></a>--}}
-{{--                            <a href="portfolio-details.html" class="details-link" title="More Details"><i--}}
-{{--                                    class="bx bx-link"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-
-{{--                    <div class="col-lg-4 col-md-6 portfolio-item filter-card">--}}
-{{--                        <img src="assets/img/portfolio/portfolio-8.jpg" class="img-fluid" alt="">--}}
-{{--                        <div class="portfolio-info">--}}
-{{--                            <h4>Card 3</h4>--}}
-{{--                            <p>Card</p>--}}
-{{--                            <a href="assets/img/portfolio/portfolio-8.jpg" data-gallery="portfolioGallery"--}}
-{{--                               class="portfolio-lightbox preview-link" title="Card 3"><i class="bx bx-plus"></i></a>--}}
-{{--                            <a href="portfolio-details.html" class="details-link" title="More Details"><i--}}
-{{--                                    class="bx bx-link"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-
-{{--                    <div class="col-lg-4 col-md-6 portfolio-item filter-web">--}}
-{{--                        <img src="assets/img/portfolio/portfolio-9.jpg" class="img-fluid" alt="">--}}
-{{--                        <div class="portfolio-info">--}}
-{{--                            <h4>Web 3</h4>--}}
-{{--                            <p>Web</p>--}}
-{{--                            <a href="assets/img/portfolio/portfolio-9.jpg" data-gallery="portfolioGallery"--}}
-{{--                               class="portfolio-lightbox preview-link" title="Web 3"><i class="bx bx-plus"></i></a>--}}
-{{--                            <a href="portfolio-details.html" class="details-link" title="More Details"><i--}}
-{{--                                    class="bx bx-link"></i></a>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-
-{{--                </div>--}}
-
-{{--            </div>--}}
-{{--        </section><!-- End Portfolio Section -->--}}
-
-        <!-- ======= Team Section ======= -->
-        <section id="team" class="team section-bg">
+        <!-- ======= Portfolio Section ======= -->
+        <section id="portfolio" class="portfolio">
             <div class="container" data-aos="fade-up">
-
                 <div class="section-title">
-{{--                    <h2>Team</h2>--}}
-                    <h3>Открыта регистрация прямо сейчас</h3>
-{{--                    <p>Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas--}}
-{{--                        atque vitae autem.</p>--}}
+                    <h2>Открыта регистрация прямо сейчас</h2>
                 </div>
-                    <div class="row">
-                        @foreach($events as $index => $event)
-                        <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
-                            <div class="member">
-                                <div class="member-img">
-                                    <a href="event/{{$event->climbing_gym_name_eng}}/{{$event->title_eng}}">
-                                        <img src="storage/{{$event->image}}" class="img-fluid" alt=""></a>
-                                    <div class="social">
-                                        <a href=""><i class="bi bi-twitter"></i></a>
-                                        <a href=""><i class="bi bi-facebook"></i></a>
-                                        <a href=""><i class="bi bi-instagram"></i></a>
-                                        <a href=""><i class="bi bi-linkedin"></i></a>
-                                    </div>
-                                </div>
-                                <div class="member-info">
-                                    <h4>{{str_replace(' ', '_',$event->city)}}</h4>
-                                </div>
+                <div class="row" data-aos="fade-up" data-aos-delay="100">
+                    <div class="col-lg-12 d-flex justify-content-center">
+                        <ul id="portfolio-flters">
+                            <li data-filter="*" class="filter-active">All</li>
+                            @foreach($cities as $city)
+                                <li data-filter=".filter-{{$city['name']}}">{{$city['name']}}<span style="margin-left: 7px; font-size: 10px;" class="badge rounded-pill bg-primary ml-5">{{$city['count_event']}}</span></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+                <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+                @foreach($events as $event)
+                        <div class="col-lg-4 col-md-6 portfolio-item filter-{{$event->city}}">
+                            <img src="storage/{{$event->image}}" class="img-fluid" alt="">
+                            <div class="portfolio-info">
+                                <h4>{{$event->city}}</h4>
+                                <a href="storage/{{$event->image}}" data-gallery="portfolioGallery"
+                                   class="portfolio-lightbox preview-link"><i class="bx bx-plus"></i></a>
+                                <a href="event/{{$event->climbing_gym_name_eng}}/{{$event->title_eng}}" class="details-link" title="More Details"><i
+                                        class="bx bx-link"></i></a>
                             </div>
                         </div>
-                        @endforeach
-                    </div>
-
+                @endforeach
+                </div>
             </div>
-        </section><!-- End Team Section -->
+        </section><!-- End Portfolio Section -->
 
 {{--        <!-- ======= Pricing Section ======= -->--}}
 {{--        <section id="pricing" class="pricing">--}}
