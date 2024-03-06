aa166c0c53ba7df9ba9ce7b7aeca573a61003e82
g.dolgushin <g.dolgushin@mcplat.ru>
2024-03-06 16:31:38 +0300

    update project

--- config/mail.php
+++ config/mail.php
@@ -36,14 +36,14 @@ return [
     'mailers' => [
 
         'smtp' => [
-            'transport' => 'mail',
+            'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.yandex.ru'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'port' => env('MAIL_PORT', 465),
+            'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'auth_mode' => "login",
+            'auth_mode' => null,
         ],
 
         'ses' => [
