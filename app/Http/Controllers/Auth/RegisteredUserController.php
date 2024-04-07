f206f52d0ea0191690090291adf0c46d4e99dc9d
g.dolgushin <g.dolgushin@mcplat.ru>
2024-04-07 18:44:22 +0300

    update project

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -38,9 +38,6 @@ class RegisteredUserController extends Controller
             'firstname' => ['required', 'string', 'max:255'],
             'lastname' => ['required', 'string', 'max:255'],
             'gender' => ['required', 'string', 'max:255'],
-            'city' => ['required', 'string', 'max:255'],
-            'birthday' => ['required', 'string'],
-            'team' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
