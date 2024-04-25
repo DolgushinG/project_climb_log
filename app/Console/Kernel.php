c82d8569dadae2ccfaad57ff779970bcce264571
g.dolgushin <40915075+DolgushinG@users.noreply.github.com>
2024-04-25 21:54:01 +0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('queue:work')->everyMinute()->withoutOverlapping();
+        $schedule->command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
+        // $schedule->command('queue:work')->everyMinute()->withoutOverlapping();
         $schedule->command('events:update-status')->hourly();
         $schedule->command('participant:update-reg-status')->hourly();
         
