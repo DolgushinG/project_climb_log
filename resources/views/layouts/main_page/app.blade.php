00fc3ba9737cb630938e28c3ebe26621dea3c9f7
g.dolgushin <g.dolgushin@mcplat.ru>
2024-05-02 15:55:18 +0300

    update project

--- resources/views/layouts/main_page/app.blade.php
+++ resources/views/layouts/main_page/app.blade.php
@@ -9,8 +9,10 @@
     <meta content="" name="keywords">
     <script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
     <!-- Favicons -->
-    <link href="{{asset('storage/img/favicon.png')}}" rel="icon">
-    <link href="{{asset('storage/img/apple-touch-icon.png')}}" rel="icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon_io/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon_io/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon_io/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{asset('favicon_io/site.webmanifest') }}">
     <!-- Google Fonts -->
 {{--    <link rel="stylesheet" href="{{asset('vendor/helpers/font-awesome.css')}}">--}}
 {{--    <script src="{{ mix('js/app.js') }}"></script>--}}
