54b6826c1be1b6b9653850767aa1bcc21c1ed319
g.dolgushin <g.dolgushin@mcplat.ru>
2024-05-14 22:54:04 +0300

    fix composer.json

--- resources/views/layouts/main_page/app.blade.php
+++ resources/views/layouts/main_page/app.blade.php
@@ -23,15 +23,15 @@
     <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{asset('vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
     <link href="{{asset('vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
-    <link href="{{asset('vendor/quill/quill.snow.css')}}" rel="stylesheet">
-    <link href="{{asset('vendor/remixicon/remixicon.css')}}" rel="stylesheet">
-    <link href="{{asset('vendor/simple-datatables/style.css')}}" rel="stylesheet">
+{{--    <link href="{{asset('vendor/quill/quill.snow.css')}}" rel="stylesheet">--}}
+{{--    <link href="{{asset('vendor/remixicon/remixicon.css')}}" rel="stylesheet">--}}
+{{--    <link href="{{asset('vendor/simple-datatables/style.css')}}" rel="stylesheet">--}}
 
-    <link href="{{asset('vendor/aos/aos.css" rel="stylesheet')}}">
+    <link href="{{asset('vendor/aos/aos.css')}}"  rel="stylesheet">
     <link href="{{asset('vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
     <link href="{{asset('vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
     <!-- Template Main CSS File -->
-    <link href="{{asset('css/style.css?v=0.8')}}" rel="stylesheet">
+    <link href="{{asset('css/style.css?v=0.10')}}" rel="stylesheet">
 
 </head>
 
@@ -44,20 +44,20 @@
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
         class="bi bi-arrow-up-short"></i></a>
 {{--<script src="{{asset('plugins/cropper/cropper.js')}}"></script>--}}
-<script src="{{asset('vendor/apexcharts/apexcharts.min.js')}}"></script>
+{{--<script src="{{asset('vendor/apexcharts/apexcharts.min.js')}}"></script>--}}
 <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('vendor/chart.js/chart.umd.js')}}"></script>
-<script src="{{asset('vendor/echarts/echarts.min.js')}}"></script>
-<script src="{{asset('vendor/quill/quill.min.js')}}"></script>
-<script src="{{asset('vendor/simple-datatables/simple-datatables.js')}}"></script>
-<script src="{{asset('vendor/tinymce/tinymce.min.js')}}"></script>
-<script src="{{asset('vendor/php-email-form/validate.js')}}"></script>
+{{--<script src="{{asset('vendor/chart.js/chart.umd.js')}}"></script>--}}
+{{--<script src="{{asset('vendor/echarts/echarts.min.js')}}"></script>--}}
+{{--<script src="{{asset('vendor/quill/quill.min.js')}}"></script>--}}
+{{--<script src="{{asset('vendor/simple-datatables/simple-datatables.js')}}"></script>--}}
+{{--<script src="{{asset('vendor/tinymce/tinymce.min.js')}}"></script>--}}
+{{--<script src="{{asset('vendor/php-email-form/validate.js')}}"></script>--}}
 <script src="{{asset('vendor/purecounter/purecounter_vanilla.js')}}"></script>
 <script src="{{asset('vendor/aos/aos.js')}}"></script>
 <script src="{{asset('vendor/glightbox/js/glightbox.min.js')}}"></script>
 <script src="{{asset('vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
 <script src="{{asset('vendor/swiper/swiper-bundle.min.js')}}"></script>
-<script src="{{asset('vendor/waypoints/noframework.waypoints.js')}}"></script>
+{{--<script src="{{asset('vendor/waypoints/noframework.waypoints.js')}}"></script>--}}
 
 <!-- Template Main JS File -->
 <script src="{{asset('js/main.js')}}"></script>
