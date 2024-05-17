3a509f65bdfac377607e01871bd1eb7854e1e979
g.dolgushin <g.dolgushin@mcplat.ru>
2024-05-17 15:18:33 +0300

    update seeder admin user

--- database/seeders/GradesSeeder.php
+++ database/seeders/GradesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Event;
 use App\Models\Grades;
+use App\Models\Route;
 use Encore\Admin\Facades\Admin;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -19,7 +20,19 @@ class GradesSeeder extends Seeder
     {
         for($i = 1; $i <= AdminRoleAndUsersSeeder::COUNT_EVENTS; $i++){
             $event = Event::find($i);
-            Grades::settings_routes($event->owner_id, $event->id, $event->count_routes);
+            $event_id = $event->id;
+            if(!$event->is_qualification_counting_like_final){
+                Route::generation_route($i, $event_id, $event->count_routes, Grades::getRoutes());
+            } else {
+                $grades = Grades::where('event_id', $event_id)->first();
+                if(!$grades){
+                    $grades = new Grades;
+                }
+                $grades->owner_id = $i;
+                $grades->event_id = $event_id;
+                $grades->count_routes = $event->count_routes;
+                $grades->save();
+            }
         }
 
 
