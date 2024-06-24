9abe252e6c1fd77b492f1847fc6856be222a5a40
g.dolgushin <g.dolgushin@mcplat.ru>
2024-06-24 11:14:20 +0300

    update style

--- resources/views/layouts/main_page/app.blade.php
+++ resources/views/layouts/main_page/app.blade.php
@@ -31,7 +31,7 @@
     <link href="{{asset('vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
     <link href="{{asset('vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
     <!-- Template Main CSS File -->
-    <link href="{{asset('css/style.css?v=0.14')}}" rel="stylesheet">
+    <link href="{{asset('css/style.css?v=0.15')}}" rel="stylesheet">
 
 </head>
 
