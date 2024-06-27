adbc3035e97de32ed9de4514b2dea80d52e6a316
g.dolgushin <g.dolgushin@mcplat.ru>
2024-06-27 17:18:05 +0300

    update result france qualification

--- app/Admin/Actions/ResultRouteFranceSystemQualificationStage/BatchResultFranceSystemQualification.php
+++ app/Admin/Actions/ResultRouteFranceSystemQualificationStage/BatchResultFranceSystemQualification.php
@@ -6,19 +6,25 @@ use App\Models\Event;
 use App\Models\Grades;
 use App\Models\ParticipantCategory;
 use App\Models\ResultFranceSystemQualification;
+use App\Models\ResultRouteFinalStage;
 use App\Models\ResultRouteFranceSystemQualification;
 use App\Models\User;
 use Encore\Admin\Actions\Action;
+use Encore\Admin\Admin;
+use Encore\Admin\Form;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class BatchResultFranceSystemQualification extends Action
 {
-    public $name = 'Внести результат финала';
+    public $name = 'Внести результат квалификации';
 
+    public function __construct()
+    {
+        $this->initInteractor();
+    }
     protected $selector = '.send-add';
-
     public function handle(Request $request)
     {
         $results = $request->toArray();
@@ -55,18 +61,27 @@ class BatchResultFranceSystemQualification extends Action
                 'Попытки на зону' => intval($results['amount_try_zone_'.$i])
             );
         }
-        $participant = ResultFranceSystemQualification::where('event_id', $results['event_id'])->where('user_id', $results['user_id'])->first();
-        $participant->active = 1;
-        $participant->result_for_edit_france_system_qualification = $result_for_edit;
-        $participant->save();
-        DB::table('result_route_france_system_qualification')->insert($data);
-        Event::refresh_france_system_qualification_counting($event);
-        return $this->response()->success('Результат успешно внесен')->refresh();
+        $result = ResultRouteFranceSystemQualification::where('event_id', $results['event_id']
+        )->where('user_id', $results['user_id'])->first();
+        $user = User::find(intval($results['user_id']))->middlename;
+        if($result){
+            return $this->response()->error('Результат уже есть по '.$user);
+        } else {
+            $participant = ResultFranceSystemQualification::where('event_id', $results['event_id'])->where('user_id', $results['user_id'])->first();
+            $participant->active = 1;
+            $participant->result_for_edit_france_system_qualification = $result_for_edit;
+            $participant->save();
+
+            DB::table('result_route_france_system_qualification')->insert($data);
+            Event::refresh_france_system_qualification_counting($event);
+            return $this->response()->success('Результат успешно внесен')->refresh();
+        }
+
     }
 
     public function form()
     {
-        $this->modalSmall();
+        $this->modalLarge();
         $event = Event::where('owner_id', '=', \Encore\Admin\Facades\Admin::user()->id)
             ->where('active', '=', 1)->first();
         $grades = Grades::where('event_id', $event->id)->first();
@@ -86,13 +101,16 @@ class BatchResultFranceSystemQualification extends Action
                 $result[$index] = $res.' ['.$category.']'.' [Уже добавлен]';
             }
         }
-        $this->select('user_id', 'Участник')->options($result)->required(true);
+
         $this->hidden('event_id', '')->value($event->id);
+        $this->hidden('event_for_style', '')->value($event->id);
+        $this->select('user_id', 'Участник')->options($result)->required(true);
         if($grades){
             for($i = 1; $i <= $grades->count_routes; $i++){
                 $this->integer('route_id_'.$i, 'Трасса')->value($i)->readOnly();
                 $this->integer('amount_try_top_'.$i, 'Попытки на топ');
                 $this->integer('amount_try_zone_'.$i, 'Попытки на зону');
+
             }
         }
 
