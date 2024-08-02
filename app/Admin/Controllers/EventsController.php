068682abebfa431161d395be2d415acbdbec04a9
g.dolgushin <g.dolgushin@mcplat.ru>
2024-08-02 16:56:05 +0300

    add new field and image upload

--- app/Admin/Controllers/EventsController.php
+++ app/Admin/Controllers/EventsController.php
@@ -616,6 +616,7 @@ class EventsController extends Controller
             $event_clone->link = $event_clone->link.'-copy';
             $event_clone->admin_link = $event_clone->admin_link.'-copy';
             $event_clone->active = 0;
+            $event_clone->is_open_send_result_state = 0;
             $event_clone->is_registration_state = 0;
             $event_clone->save();
 
