baf5f2ba0a566774838fe9e95f3c9c89ed4765a9
g.dolgushin <40915075+DolgushinG@users.noreply.github.com>
2024-08-05 22:53:11 +0300

    fix and turn off cookies, fix text in profile (#112)
    Co-authored-by: g.dolgushin <g.dolgushin@mcplat.ru>

--- resources/views/cookie/cookies.blade.php
+++ resources/views/cookie/cookies.blade.php
@@ -3,8 +3,8 @@
         <div class="text">Мы сохраняем файлы cookie<br>(всё <a class="text text_view_link" href="{{route('privacyconf')}}" target="_blank">по правилам конфиденциальности</a>)
         </div>
     </div>
-    <div class="cookie-policy__ok-btn i-bem cookie-policy__ok-btn_js_inited">
-        <div id="cookie_agree" style="color: white" class="text text_view_link text_size_m">OK</div>
+    <div class="cookie-policy__ok-btn i-bem cookie-policy__ok-btn_js_inited mt-2">
+        <div id="cookie_agree" style="color: white" class="btn btn-outline-primary text text_view_link text_size_m">OK</div>
     </div>
 </div>
 <script src="{{asset('js/pages/js.cookie.js')}}"></script>
@@ -15,7 +15,6 @@
         Cookies.set('cookie_agreement', 'false');
     }
     $('#cookie_agree').click(function(){
-
         Cookies.set('cookie_agreement', 'true');
         $('.cookie-policy__card').attr('style', 'display:none');
     });
