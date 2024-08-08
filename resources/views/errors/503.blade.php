4064b03aa97524c6526e13b944bb0feab32a10bc
g.dolgushin <g.dolgushin@mcplat.ru>
2024-08-08 17:22:43 +0300

    new type payments

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.main_page.app')
+@section('content')
+    <section id="about" class="about">
+        <div class="container" data-aos="fade-up" style="margin-top: 4rem;">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                            <!-- Tabs Titles -->
+                            <!-- Icon -->
+                            <div class="fadeIn first" style="padding-top: 17px;">
+                                <div class="container"><i class="fa fa-cogs" style="font-size: 200px; color: #0a53be">  </i><span style="font-size: 200px">503</span></div>
+                                <span>Вот это удача попали на обновление сайта!</span><br>
+                                <span>Почему ошибка? Дело в том что обновление и одновременная работа с сайтом, может привести </span>
+                                <span>к не предвиденным ошибкам, поэтому сайт нужно перевести в особый режим работы.</span><br>
+                                <span>В целом все хорошо, через 5-10 секунда все заработает, наверно =) !</span>
+                                <span>Если нет пишем <a href="https://t.me/gdolgushin">сюда</a></span>
+
+                            </div>
+                            <!-- Login Form -->
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
