726086de62cfceea69378a0f7467a957728b25e7
g.dolgushin <40915075+DolgushinG@users.noreply.github.com>
2024-10-12 16:22:15 +0300

    fix payment and update pay for event and export list (#166)
    Co-authored-by: g.dolgushin <g.dolgushin@mcplat.ru>

--- app/Models/OwnerPaymentOperations.php
+++ app/Models/OwnerPaymentOperations.php
@@ -28,34 +28,36 @@ class OwnerPaymentOperations extends Model
         }
     }
 
-    public static function execute_payment($participant, $admin, $event, $amount_participant)
+    public static function execute_payment($participant, $owner_id, $event, $amount_participant)
     {
         $payments = OwnerPayments::where('event_id', $participant->event_id)->first();
         if (!$payments) {
             $payments = new OwnerPayments;
+            $payments->owner_id = $owner_id;
+            $payments->event_id = $participant->event_id;
+            $payments->event_title = $event->title;
         }
         $amount = OwnerPaymentOperations::where('event_id', $participant->event_id)->sum('amount');
-        $payments->owner_id = $admin->id;
-        $payments->event_id = $participant->event_id;
-        $payments->event_title = $event->title;
         $payments->amount_for_pay = $amount;
         $payments->amount_participant = $amount_participant;
         $payments->amount_cost_for_service = Event::COST_FOR_EACH_PARTICIPANT;
         $payments->save();
     }
 
-    public static function execute_payment_operations($participant, $admin, $amount_start_price, $amount_name)
+    public static function execute_payment_operations($participant, $owner_id, $amount_start_price, $amount_name = null)
     {
         $transaction = OwnerPaymentOperations::where('event_id', $participant->event_id)
             ->where('user_id', $participant->user_id)->first();
         if (!$transaction) {
             $transaction = new OwnerPaymentOperations;
+            $transaction->owner_id = $owner_id;
+            $transaction->user_id = $participant->user_id;
+            $transaction->event_id = $participant->event_id;
         }
-        $transaction->owner_id = $admin->id;
-        $transaction->user_id = $participant->user_id;
-        $transaction->event_id = $participant->event_id;
         $transaction->amount = Event::counting_amount_for_pay_participant($amount_start_price);
-        $transaction->type = $amount_name;
+        if($amount_name){
+            $transaction->type = $amount_name;
+        }
         $transaction->save();
     }
 }
